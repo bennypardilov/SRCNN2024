aa4f497f60950bcdb31d82dccb743fdd6ed05a1f
bennypardilov <80034677+bennypardilov@users.noreply.github.com>
2024-05-22 21:18:22 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms.functional as TF
+from torch.utils.data import TensorDataset, DataLoader
+
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
